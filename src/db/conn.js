494da964fb8fed6da1eaa4ae885db62b126ef7fb
Yogesh1738 <yogeshrajoria1619@gmail.com>
2025-04-09 00:05:04 +0530

    second commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 //connection creation and creatin a new db
-mongoose.connect("mongodb+srv://ashman_jagdev:ashmanraju@cluster0.n8apx.mongodb.net/BlogsDB",{useNewUrlParser: true,useUnifiedTopology: true})
-.then(()=> console.log("connection successful...."))
+mongoose.connect("/BlogsDB",{useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=> console.log("ccessful...."))
 .catch((err)=>console.log(err));
